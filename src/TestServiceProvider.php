3d8e98b5d6d102117f9b7bbc6a3bd94f089e92d4
sunzhiping <szp043725449@126.com>
2016-02-24 14:49:31 +0800

    ss

---
+++ src/TestServiceProvider.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace Test\Api;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider {
+
+    /**
+     * Boot service provider.
+     */
+    public function boot()
+    {
+        
+    }
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+       
+    }
+}
